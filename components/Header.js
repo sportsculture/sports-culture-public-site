9d0f30a5dff382d6b38086dbcd90a5bb72b8438a
M Chambers <origin@graybeam.tech>
2023-10-03 09:40:09 -0500

    centered image and using fill in Image and a surrounding div

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 import Image from 'next/image'
-import profilePic from '/assets/images/Sports_Culture_PNG.png'
+import logoImage from '/assets/images/Sports_Culture_PNG.png'
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-        <Image src={profilePic} className="w-12 h-12 block mx-auto mb-4" width={100} height={90}/>
+        <div className="w-36 h-24 block mx-auto mb-4">
+            <Image src={logoImage} fill/>
+        </div>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
