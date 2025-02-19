981604492091f72cae12be5f0907abf5bf75d6b1
M Chambers <origin@graybeam.tech>
2025-02-18 22:53:50 -0600

    fix: Update Next.js Image component, Cypress tests, and Netlify config

--- components/Header.js
+++ components/Header.js
@@ -5,8 +5,17 @@ import logoImage from '/assets/images/Sports_Culture_PNG.png'
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-        <div className="w-36 h-24 block mx-auto mb-4">
-            <Link href="/" ><Image src={logoImage} fill/></Link>
+        <div className="w-36 h-24 relative block mx-auto mb-4">
+            <Link href="/">
+              <Image 
+                src={logoImage} 
+                alt="Sports Culture Logo"
+                fill="true"
+                className="object-contain"
+                sizes="(max-width: 768px) 144px, 144px"
+                priority
+              />
+            </Link>
         </div>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,9 +1,27 @@
-describe('sample render test', () => {
+describe('Sports Culture Website Tests', () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
-  it('displays the title text', () => {
-    cy.get('h1').contains('Next.js Blog Theme');
+  it('displays the correct site title and tagline', () => {
+    cy.get('h1').contains('Making the world a better place through sports.');
+  });
+
+  it('displays the header with site name', () => {
+    cy.get('header').contains('Sports Culture');
+  });
+
+  it('displays blog posts', () => {
+    cy.get('ul li').should('have.length.at.least', 1);
+    cy.get('h2').should('exist');
+  });
+
+  it('has working post links', () => {
+    cy.get('ul li a').first().click();
+    cy.url().should('include', '/posts/');
+  });
+
+  it('has footer with copyright text', () => {
+    cy.get('footer').contains('All rights reserved');
   });
 });
